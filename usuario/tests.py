919d1a467a7c93836a445b53b4016601b0926f9b
Miguel Ramos <miguelvieiraramos02@gmail.com>
2021-01-22 09:03:18 -0300

    code convention

--- usuario/tests.py
+++ usuario/tests.py
@@ -28,10 +28,10 @@ class ViewTestCase(TestCase):
     def test_api_can_create_usuario(self):
         self.response = self.client.post('/usuarios/', self.usuario_data, format='json')
         assert self.response.status_code == status.HTTP_201_CREATED
-    
+
     def test_api_can_list_all_usuarios(self):
-      self.response = self.client.get('/usuarios/')
-      assert self.response.status_code == status.HTTP_200_OK
+        self.response = self.client.get('/usuarios/')
+        assert self.response.status_code == status.HTTP_200_OK
 
     def test_unique_username_validator(self):
         self.user = User.objects.create(username='foo', email='foo@email.com')
@@ -40,5 +40,3 @@ class ViewTestCase(TestCase):
         self.usuario.save()
         self.response = self.client.post('/usuarios/', self.usuario_data, format='json')
         assert self.response.status_code == status.HTTP_400_BAD_REQUEST
-
-
